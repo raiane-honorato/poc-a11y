74fc0502282074fb03d5893ea4d469533680f9f2
raiane-honorato <raianehonorato16@gmail.com>
2022-06-04 16:10:02 -0300

    feat(a11y): add function to remove tabindex when popup

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const menuBtn = document.querySelector('#menu-btn');
 const menu = document.querySelector('#menu');
 const tabIndexItemList = ['#menu-btn', ".header__home-link", '.main-section__btn'];
+const tabIndexItemList2 = ['#menu-btn', ".header__home-link", '.main-section__btn', "#menu button", "#menu a"];
 
 const handleExpandAndClose = (isOpen, clickElement, expandElement, className = '') => {
 
@@ -39,31 +40,33 @@ const handleOutsideClick = (element, btn = [], callbackFunction = () => {}) => {
   document.body.addEventListener("click", onClick);
 }
 
-const handleTabIndexOutsideMenu = () => {
+const handleTabIndexOutsideMenu = (tabIndexItemList) => {
   tabIndexItemList.forEach((query) => {
-    let element = document.querySelector(query);
-    let elementTabIndex = element.tabIndex;
-    if(elementTabIndex === 0){
-      element.setAttribute("tabIndex", -1);
-      return null;
-    }
-    if(elementTabIndex === -1){
-      element.setAttribute("tabIndex", 0);
-      return null;
-    }
+    let elements = document.querySelectorAll(query);
+    elements.forEach(element => {
+      let elementTabIndex = element.tabIndex;
+      if(elementTabIndex === 0){
+        element.setAttribute("tabIndex", -1);
+        return null;
+      }
+      if(elementTabIndex === -1){
+        element.setAttribute("tabIndex", 0);
+        return null;
+      }
+    })
   })
 }
 
 menuBtn.addEventListener('click', () => {
   const isOpen = JSON.parse(menuBtn.getAttribute('aria-expanded'));
   handleExpandAndClose(isOpen, menuBtn, menu, 'menu-transition');
-  handleTabIndexOutsideMenu();
+  handleTabIndexOutsideMenu(tabIndexItemList);
 });
 
 menu.addEventListener('click', (e) => {
   if(e.offsetX < 0){
     handleExpandAndClose(true, menuBtn, menu, 'menu-transition');
-    handleTabIndexOutsideMenu();
+    handleTabIndexOutsideMenu(tabIndexItemList);
   }
 });
 
@@ -71,7 +74,7 @@ const closeMenuBtn = document.querySelector('#close-menu-btn');
 
 closeMenuBtn.addEventListener('click', () => {
   handleExpandAndClose(true, menuBtn, menu, 'menu-transition');
-  handleTabIndexOutsideMenu();
+  handleTabIndexOutsideMenu(tabIndexItemList);
 });
 
 const menuDropdownBtn = document.querySelectorAll('.navigation__menu-btn-dropdown');
@@ -101,13 +104,17 @@ const popupCloseBtn = document.querySelector(".popup-wrap__close-btn");
 
 learnButton.addEventListener('click', () => {
   handleExpandAndClose(false, learnButton, popupSection, 'popup-section--transition');
-  handleOutsideClick(popupSection.children[0], [learnButton,popupCloseBtn], () => {handleExpandAndClose(true, learnButton, popupSection, 'popup-section--transition')});
+  handleTabIndexOutsideMenu(tabIndexItemList2);
+  handleOutsideClick(popupSection.children[0], [learnButton,popupCloseBtn], () => {
+    handleExpandAndClose(true, learnButton, popupSection, 'popup-section--transition');
+    handleTabIndexOutsideMenu(tabIndexItemList2);
+  });
 
 });
 
-
 popupCloseBtn.addEventListener("click", () => {
   handleExpandAndClose(true, learnButton, popupSection, 'popup-section--transition');
+  handleTabIndexOutsideMenu(tabIndexItemList2);
 });
 
 
